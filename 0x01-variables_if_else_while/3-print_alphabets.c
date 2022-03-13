e7bd005522f6dff499d7659bf92cd05976e13ec2
eyosiyasl <eyosiyas247@gmail.com>
2022-03-13 03:41:48 -0700

    mess

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,18 @@
  */
 int main(void)
 {
-	char lower_case;
-	char upper_case;
+char lower_case;
+char upper_case;
 
-	for (lower_case = 'a'; lower_case <= 'z'; lowercase++)
-		putchar(lowercase);
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-		putchar(upper_case);
+for (lower_case = 'a'; lower_case <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+putchar('\n');
 
-		putchar('\n');
-
-	return (0);
+return (0);
 }
