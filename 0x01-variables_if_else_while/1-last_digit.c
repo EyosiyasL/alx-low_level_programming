cb87cf2f0bb9a3f2ec58be9d4b7940a2ee1260e5
eyosiyasl <eyosiyas247@gmail.com>
2022-03-13 05:31:02 +0000

    1-lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success)    
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 	int L;
 	char str[] = "Last digit of";
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	L = n % 10;
