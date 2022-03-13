276e8b999cb38caddbcc5e67f18be2bbac13af3a
eyosiyasl <eyosiyas247@gmail.com>
2022-03-13 03:14:54 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar("\n");
-	
-	return (0);
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+{
+putchar(low);
+}
+
+putchar("\n");
+
+return (0);
+
 }
