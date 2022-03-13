40cbd885bcc71978714320617ab7ae213ccb946d
eyosiyasl <eyosiyas247@gmail.com>
2022-03-13 06:03:48 -0700

    Print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@ int main(void)
 {
 int number;
 
-for (number = 0; number <= 9; number++)
+for (number = '0'; number <= '9'; number++)
 {
 putchar(number);
 
-if (number == 9)
+if (number == '9')
 {
 break;
 }
