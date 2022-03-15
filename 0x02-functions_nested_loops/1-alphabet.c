76ca16e5d0621e2daa4931f82d77848248e4ca36
eyosiyasl <eyosiyas247@gmail.com>
2022-03-15 16:49:54 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char d;
+
+	for (d = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n')
+}
