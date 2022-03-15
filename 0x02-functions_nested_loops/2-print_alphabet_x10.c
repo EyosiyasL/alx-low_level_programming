01e6b301c6b32391a7b5c09fee04c161ca17fbca
eyosiyasl <eyosiyas247@gmail.com>
2022-03-15 17:00:14 +0000

    Print Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0.
  */
+
 void print_alphabet_x10(void)
 {
 	int i;
-	char c;
+	char d;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
+		for (d = 'a'; d <= 'z'; d++)
+			_putchar(d);
 		_putchar('\n');
 	}
 }
