9431e71fd777879bbf111da804dfebe5b3a0dce8
eyosiyasl <eyosiyas247@gmail.com>
2022-03-15 17:16:55 +0000

    Function Nested

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - returns 1 if lowercase alphabet and 0 otherwise
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+
+int _islower(int d)
+{
+	if (d >= 'a' && d <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
